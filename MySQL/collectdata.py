5adae105f2911fb6198ee5e861cf3408219709f1
mmalagride <mmalagride@gms.ca>
2022-11-29 10:12:48 -0600

    Added log file and scheduled script

--- MySQL/collectdata.py
+++ MySQL/collectdata.py
@@ -4,26 +4,21 @@ from DataCollector import gameinfo
 from DataCollector import preparedata
 from DataCollector import championkill
 from DataCollector import __collectorFunctions
+from datetime import datetime
 import database
 import os
 import json
 import logging
 import time
 
-def getRiotApi():
-    keyPath = os.path.dirname(os.path.dirname(__file__)) + "\\Secrets\\riot-api-key.json"
-    key = open(keyPath, 'r')
-    content = json.loads(key.read())
-    return content['api_key']
-
 if __name__ == "__main__":
     start_time = time.time()
-    logging.basicConfig(filename=os.path.dirname(sys.argv[0])+os.sep+'mylog.log', level=logging.DEBUG)
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(filename=os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/Logs/MySQL.log', level=logging.DEBUG, filemode='w')
+    logging.info("Execution time: " + datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     api_key = __collectorFunctions.getRiotApi()
     db = 'loldata'
     daysAgo = 1
-    maxGames = 100
+    maxGames = 50
     database.refreshDatabase(db)
     friendinfo.CollectFriendInfo(api_key, db)
     gameids.CollectGameIds(api_key, db, daysAgo)
