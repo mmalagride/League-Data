a62d58e5c7b644d15d6b02e87f46909633048404
mmalagride <mmalagride@gms.ca>
2023-02-07 21:47:51 -0600

    Created Second smaller dataset for quicker use

--- MySQL/DataCollector/preparedata.py
+++ MySQL/DataCollector/preparedata.py
@@ -10,6 +10,7 @@ def LoadFreshData(sheetname, db):
     logging.info("Loading Game data into Googlesheets...")
     DW = __collectorFunctions.ConnectDB(db)
     gc = pygsheets.authorize(service_file=os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "\\Secrets\\google-api-key.json")
+    logging.info("Loading data for complete datset...")
     logging.info("Executing fullgamedetails query...")
     dataframe1 = pd.read_sql('SELECT * FROM fullgamedetails', con=DW['connection'])
     sh = gc.open(sheetname)
@@ -20,7 +21,20 @@ def LoadFreshData(sheetname, db):
     dataframe2 = pd.read_sql('SELECT * FROM killdetails', con=DW['connection'])
     wks2 = sh[1]
     wks2.clear()
-    wks2.set_dataframe(dataframe2,(1,1))    
+    wks2.set_dataframe(dataframe2,(1,1))
+    logging.info("Load complete!")
+    logging.info("Loading data for 3 month subset...")
+    logging.info("Executing fullgamedetails query...")
+    dataframe1 = pd.read_sql('SELECT * FROM fullgamedetails where gameStartTime > ((unix_timestamp() * 1000) - 7776000000);', con=DW['connection'])
+    sh3 = gc.open(sheetname + " - 3 Months")
+    wks1 = sh3[0]
+    wks1.clear()
+    wks1.set_dataframe(dataframe1,(1,1))
+    logging.info("Executing killdetails query...")
+    dataframe2 = pd.read_sql('select distinct kd.* from killdetails kd left join fullgamedetails fgd on kd.gameID = fgd.gameID where fgd.gameStartTime > ((unix_timestamp() * 1000) - 7776000000);', con=DW['connection'])
+    wks2 = sh3[1]
+    wks2.clear()
+    wks2.set_dataframe(dataframe2,(1,1))   
     logging.info("Load complete! Dashboard is now up-to-date!")
 
 
